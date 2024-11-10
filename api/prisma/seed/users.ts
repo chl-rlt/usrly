cc933045d2381f7a0220471b541c9c7bcb6f2c1d
chloe <chloe@acta-ds.fr>
2024-11-10 23:48:52 +0100

    refactor(database): :seedling: update users seed

--- api/prisma/seed/users.ts
+++ api/prisma/seed/users.ts
@@ -18,7 +18,11 @@ export async function seedUsers() {
       const firstName = faker.person.firstName();
       const lastName = faker.person.lastName();
       const email = faker.internet.email({ firstName, lastName });
-      const password = await hashPassword(faker.internet.password());
+      const password = await hashPassword(
+        faker.internet.password({
+          length: 10,
+        })
+      );
       const birthDate = faker.date.birthdate({
         mode: "year",
         min: 1960,
@@ -32,6 +36,8 @@ export async function seedUsers() {
   await UserFactory.create({
     email: "dev@example.com",
     password: await hashPassword("dev123"),
+    firstName: "Dev",
+    lastName: "User",
   });
 
   await UserFactory.createList(30);
